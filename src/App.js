dd64859c89387c7456e5c09b49d41b1baca48232
nerlijma <nerlijma@gmail.com>
2021-10-23 19:20:10 -0300

    added styled collection item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import HomePage from './homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+
   );
 }
 
