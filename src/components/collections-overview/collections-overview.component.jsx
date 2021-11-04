961512f4815c36c8a4eac632a811c478e98fe46a
nerlijma <nerlijma@gmail.com>
2021-11-04 12:02:22 -0300

    Added reducer for collectionAsArray

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsAsArray } from '../../redux/shop/shop.selectors';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -11,19 +11,17 @@ const CollectionsOverview = ({ collections }) => {
         <div className='collections-overview'>
             <div>Collections</div>
             {
-                Object.keys(collections).map((key) => {
-                    const item = collections[key];
-                    return (
-                        <CollectionPreview key={item.id} {...item} />
-                    )
-                })
+                collections.map(({ id, ...otherCollectionProps }) =>
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                )
+
             }
         </div>
     );
 }
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsAsArray
 })
 
 export default connect(
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,6 +8,11 @@ export const selectCollections = createSelector(
     (shop) => shop.collections
 );
 
+export const selectCollectionsAsArray = createSelector(
+    [selectCollections],
+    (collections) => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = collectionId => createSelector(
     [selectCollections],
     (collections) => collections[collectionId]
