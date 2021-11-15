20dada7912e7f1ba7e535ccc9db7b69c7853e842
nerlijma <nerlijma@gmail.com>
2021-11-15 15:56:43 -0300

    converted CartDropDown to useHistory

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import { setToogleVisibility } from '../../redux/cart/cart.actions'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
